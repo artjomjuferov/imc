3397ad0c7a230163194e1cd4e66cdf9ca312e78c
Artjom Juferov <artjom@toptal.com>
2019-03-15 22:49:25 +0200

    Commit forgotten spec

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Location, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_numericality_of(:long).is_greater_than(-180) }
+    it { is_expected.to validate_numericality_of(:long).is_less_than(180) }
+
+    it { is_expected.to validate_numericality_of(:lat).is_greater_than(-90) }
+    it { is_expected.to validate_numericality_of(:lat).is_less_than(90) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:hub) }
+  end
+end
