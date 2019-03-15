27ce716422efb7fde0db3cd89e6f9bc85a51b38e
Artjom Juferov <artjom@toptal.com>
2019-03-15 23:16:29 +0200

    Add validations to Hub

--- app/models/hub.rb
+++ app/models/hub.rb
@@ -1,3 +1,12 @@
 class Hub < ApplicationRecord
+  STATUSES = %w[AA AC AF AI AM AQ AS RL RN RQ UR RR QQ XX]
+
+  validates :name, presence: true, uniqueness: true
+  validates :name_wo_diacritics, presence: true, uniqueness: true
+  validates :subdiv, presence: true
+  validates :status, presence: true, inclusion: { in: STATUSES }
+  validates :uploaded_date, presence: true
+
   belongs_to :country
+  has_one :location
 end
---
+++ spec/models/hub_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe Hub, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+
+    it { is_expected.to validate_presence_of(:name_wo_diacritics) }
+    it { is_expected.to validate_uniqueness_of(:name_wo_diacritics) }
+
+    it { is_expected.to validate_presence_of(:subdiv) }
+
+    it { is_expected.to validate_presence_of(:status) }
+    it { is_expected.to validate_inclusion_of(:status).in_array(%w[AA AC AF AI AM AQ AS RL RN RQ UR RR QQ XX]) }
+
+    it { is_expected.to validate_presence_of(:uploaded_date) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:country) }
+    it { is_expected.to have_one(:location) }
+  end
+end
