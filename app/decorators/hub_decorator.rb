657542069599f49b927672f4619be167bfeca547
Artjom Juferov <artjom@toptal.com>
2019-03-16 22:05:24 +0200

    Implement HubDecorator#change_code_humane

--- app/decorators/hub_decorator.rb
+++ app/decorators/hub_decorator.rb
@@ -2,4 +2,21 @@ class HubDecorator < Draper::Decorator
   def unlocode
     "#{object.country.code} #{object.code}"
   end
+
+  def change_code_humane
+    case object.change_code
+    when 'X'
+      'Marked for deletion in the next issue'
+    when '#'
+      'Change in location name (usually spelling)'
+    when '¦'
+      'Other changes in the entry (not location)'
+    when '+'
+      'Entry added to the current issue'
+    when '='
+      'Reference entry'
+    when '!'
+      'Retained for certain entries in the USA code list ("controlled duplications")'
+    end
+  end
 end
--- spec/decorators/hub_decorator_spec.rb
+++ spec/decorators/hub_decorator_spec.rb
@@ -3,9 +3,45 @@ require 'rails_helper'
 RSpec.describe HubDecorator, type: :decorator do
   describe '#unlocode' do
     subject { hub.decorate.unlocode }
-    let(:country) { create(:country, name: 'Germany', code: 'DE') }
-    let(:hub) { create(:hub, country: country, code: 'HAM') }
+    let(:country) { build(:country, name: 'Germany', code: 'DE') }
+    let(:hub) { build(:hub, country: country, code: 'HAM') }
 
     it { is_expected.to eq('DE HAM') }
+  end
+
+  describe '#change_code_humane' do
+    subject { hub.decorate.change_code_humane }
+
+    let(:hub) { build(:hub, change_code: change_code) }
+
+    context 'with X change_code' do
+      let(:change_code) { 'X' }
+      it { is_expected.to eq('Marked for deletion in the next issue') }
+    end
+
+    context 'with # change_code' do
+      let(:change_code) { '#' }
+      it { is_expected.to eq('Change in location name (usually spelling)') }
+    end
+
+    context 'with ¦ change_code' do
+      let(:change_code) { '¦' }
+      it { is_expected.to eq('Other changes in the entry (not location)') }
+    end
+
+    context 'with + change_code' do
+      let(:change_code) { '+' }
+      it { is_expected.to eq('Entry added to the current issue') }
+    end
+
+    context 'with = change_code' do
+      let(:change_code) { '=' }
+      it { is_expected.to eq('Reference entry') }
+    end
+
+    context 'with ! change_code' do
+      let(:change_code) { '!' }
+      it { is_expected.to eq('Retained for certain entries in the USA code list ("controlled duplications")') }
+    end
   end
 end
