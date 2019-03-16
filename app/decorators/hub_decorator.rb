858747893eb8b43e3953d13f68d3940bbfef2e69
Artjom Juferov <artjom@toptal.com>
2019-03-16 22:35:45 +0200

    Implement HubDecorator#change_code_humane

--- app/decorators/hub_decorator.rb
+++ app/decorators/hub_decorator.rb
@@ -19,4 +19,37 @@ class HubDecorator < Draper::Decorator
       'Retained for certain entries in the USA code list ("controlled duplications")'
     end
   end
+
+  def satatus_humane
+    case object.status
+    when 'AA'
+      'Approved by competent national government agency'
+    when 'AC'
+      'Approved by Customs Authority'
+    when 'AF'
+      'Approved by national facilitation body'
+    when 'AI'
+      'Code adopted by international organisation (IATA or ECLAC)'
+    when 'AM'
+      'Approved by the UN/LOCODE Maintenance Agency'
+    when 'AQ'
+      'Entry approved, functions not verified'
+    when 'AS'
+      'Approved by national standardisation body'
+    when 'RL'
+      'Recognised location - Existence and representation of location name confirmed by check against nominated gazetteer or other reference work'
+    when 'RN'
+      'Request from credible national sources for locations in their own country'
+    when 'RQ'
+      'Request under consideration'
+    when 'UR'
+      "Entry included on user's request; not officially approved"
+    when 'RR'
+      'Request rejected'
+    when 'QQ'
+      'Original entry not verified since date indicated'
+    when 'XX'
+      'Entry that will be removed from the next issue of UN/LOCODE'
+    end
+  end
 end
--- spec/decorators/hub_decorator_spec.rb
+++ spec/decorators/hub_decorator_spec.rb
@@ -44,4 +44,80 @@ RSpec.describe HubDecorator, type: :decorator do
       it { is_expected.to eq('Retained for certain entries in the USA code list ("controlled duplications")') }
     end
   end
+
+  describe '#satatus_humane' do
+    subject { hub.decorate.satatus_humane }
+
+    let(:hub) { build(:hub, status: status) }
+
+    context 'with AA status' do
+      let(:status) { 'AA' }
+      it { is_expected.to eq('Approved by competent national government agency') }
+    end
+
+    context 'with AC status' do
+      let(:status) { 'AC' }
+      it { is_expected.to eq('Approved by Customs Authority') }
+    end
+
+    context 'with AF status' do
+      let(:status) { 'AF' }
+      it { is_expected.to eq('Approved by national facilitation body') }
+    end
+
+    context 'with AI status' do
+      let(:status) { 'AI' }
+      it { is_expected.to eq('Code adopted by international organisation (IATA or ECLAC)') }
+    end
+
+    context 'with AM status' do
+      let(:status) { 'AM' }
+      it { is_expected.to eq('Approved by the UN/LOCODE Maintenance Agency') }
+    end
+
+    context 'with AQ status' do
+      let(:status) { 'AQ' }
+      it { is_expected.to eq('Entry approved, functions not verified') }
+    end
+
+    context 'with AS status' do
+      let(:status) { 'AS' }
+      it { is_expected.to eq('Approved by national standardisation body') }
+    end
+
+    context 'with RL status' do
+      let(:status) { 'RL' }
+      it { is_expected.to eq('Recognised location - Existence and representation of location name confirmed by check against nominated gazetteer or other reference work') }
+    end
+
+    context 'with RN status' do
+      let(:status) { 'RN' }
+      it { is_expected.to eq('Request from credible national sources for locations in their own country') }
+    end
+
+    context 'with RQ status' do
+      let(:status) { 'RQ' }
+      it { is_expected.to eq('Request under consideration') }
+    end
+
+    context 'with UR status' do
+      let(:status) { 'UR' }
+      it { is_expected.to eq("Entry included on user's request; not officially approved") }
+    end
+
+    context 'with RR status' do
+      let(:status) { 'RR' }
+      it { is_expected.to eq('Request rejected') }
+    end
+
+    context 'with QQ status' do
+      let(:status) { 'QQ' }
+      it { is_expected.to eq('Original entry not verified since date indicated') }
+    end
+
+    context 'with XX status' do
+      let(:status) { 'XX' }
+      it { is_expected.to eq('Entry that will be removed from the next issue of UN/LOCODE') }
+    end
+  end
 end
