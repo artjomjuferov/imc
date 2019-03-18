0a7bd3d38748c3a621f5ab4445cdda89f81732d7
Artjom Juferov <artjom@toptal.com>
2019-03-18 12:19:02 +0200

    Add postgis to table

---
+++ db/migrate/20190317210724_remove_lat_and_long_from_locations.rb
@@ -0,0 +1,6 @@
+class RemoveLatAndLongFromLocations < ActiveRecord::Migration[5.1]
+  def change
+    # remove_column :locations, :lat
+    # remove_column :locations, :long
+  end
+end
---
+++ db/migrate/20190317211051_add_longlat_to_locations.rb
@@ -0,0 +1,6 @@
+class AddLonglatToLocations < ActiveRecord::Migration[5.1]
+  def change
+    # add_column :locations, :longlat, :st_point, geographic: true
+    # add_index :locations, :longlat, using: :gist
+  end
+end
