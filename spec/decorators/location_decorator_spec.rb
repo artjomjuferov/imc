28fe76633674793835b94b45018ae7aa9dd9cbcd
Artjom Juferov <derevjaginti@gmail.com>
2019-03-18 13:12:00 +0000

    Remove not needed lat, long

--- spec/decorators/location_decorator_spec.rb
+++ spec/decorators/location_decorator_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe LocationDecorator, type: :decorator do
   describe '#map_link' do
     subject { location.decorate.map_link }
     let(:hub) { build(:hub) }
-    let(:location) { Location.new(hub: hub, lat: 1.0, long: 2.0, longlat: 'POINT(2 1)') }
+    let(:location) { Location.new(hub: hub, longlat: 'POINT(2 1)') }
 
     it { is_expected.to eq("<a href='https://www.google.com/maps/search/?api=1&query=1.0,2.0'>Show on map</a>") }
   end
